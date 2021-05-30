46aefafa70c030be012c029e7579c05087911315
awaly.mohamad@gmail.com <awaly.mohamad@gmail.com>
2021-05-30 21:35:16 +0200

    FActory

---
+++ src/main/java/be/atc/controler/connexion/Factory.java
@@ -0,0 +1,53 @@
+package be.atc.controler.connexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Classe qui permet de faire la connexion a la DB
+ */
+
+public class Factory {
+    private String url;
+    private String username;
+    private String password;
+
+    Factory( String url, String username, String password ) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Méthode qui charge le driver JDBC
+     * se connecte a la base de données
+     *
+     * @return
+     */
+    public static Factory getInstance() {
+        try {
+            Class.forName( "com.mysql.jdbc.Driver" );
+        } catch ( ClassNotFoundException e ) {
+            e.printStackTrace();
+        }
+        Factory instance = new Factory(
+                "jdbc:mysql://localhost:3306/stockManagement", "Administrateur", "Admin"
+        );
+        return instance;
+    }
+
+    /**
+     * Méthode qui permet a tout moment de recupere la connexion a la base de données
+     * quand on l'utilisera dans l'implementation
+     *
+     * @return
+     * @throws SQLException
+     */
+    public Connection getConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection( url, username, password );
+        connection.setAutoCommit( false );
+        return connection;
+
+    }
+}
